93f501d2e550f114ae93041480f91609f2b5fee7
VasyaBerdar <vasylberdar09@gmail.com>
2025-12-04 23:14:02 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ namespace LabRobOPAME3
 {
     internal class Program
     {
+        private static List<Product> products = new List<Product>();
         class Product
         {
             public string Name = "";      
@@ -53,18 +54,13 @@ namespace LabRobOPAME3
 
         private static void MainMenu()
         {
-            Product[] products = new Product[10];
+            products = new List<Product>();
 
-            products[0] = new Product() { Name = "Футболка", Price = 450, Quantity = 25, Category = "Одяг" };
-            products[1] = new Product() { Name = "Джинси", Price = 1200, Quantity = 18, Category = "Одяг" };
-            products[2] = new Product() { Name = "Кросівки", Price = 2500, Quantity = 12, Category = "Взуття" };
-            products[3] = new Product() { Name = "Куртка", Price = 3200, Quantity = 10, Category = "Одяг" };
-            products[4] = new Product() { Name = "Шорти", Price = 600, Quantity = 30, Category = "Одяг" };
-            products[5] = new Product() { Name = "Кофта", Price = 850, Quantity = 22, Category = "Одяг" };
-            products[6] = new Product() { Name = "Бомбер", Price = 2800, Quantity = 8, Category = "Одяг" };
-            products[7] = new Product() { Name = "Шкарпетки", Price = 80, Quantity = 100, Category = "Аксесуари" };
-            products[8] = new Product() { Name = "Сорочка", Price = 900, Quantity = 20, Category = "Одяг" };
-            products[9] = new Product() { Name = "Черевики", Price = 3400, Quantity = 14, Category = "Взуття" };
+            products.Add(new Product() { Name = "Футболка", Price = 450, Quantity = 25, Category = "Одяг" });
+            products.Add( new Product() { Name = "Джинси", Price = 1200, Quantity = 18, Category = "Одяг" });
+            products.Add( new Product() { Name = "Кросівки", Price = 2500, Quantity = 12, Category = "Взуття" });
+            products.Add(new Product() { Name = "Куртка", Price = 3200, Quantity = 10, Category = "Одяг" });
+            products.Add(new Product() { Name = "Шапка", Price = 200, Quantity = 10, Category = "Одяг" });
 
 
             while (true)
@@ -73,8 +69,12 @@ namespace LabRobOPAME3
                 Console.WriteLine("1. Показати всі товари");
                 Console.WriteLine("2. Купити товар");
                 Console.WriteLine("3. Статистика");
-                Console.WriteLine("4. Звіт");
-                Console.WriteLine("5. Вихід");
+                Console.WriteLine("4. Додати товар");
+                Console.WriteLine("5. Пошук");
+                Console.WriteLine("6. Видалення");
+                Console.WriteLine("7. Сортування за назвою");
+                Console.WriteLine("8. Сортування бульбашкою");
+                Console.WriteLine("0. Вихід");
 
                 Console.Write("Ваш вибір: ");
                 string choice = Console.ReadLine();
@@ -82,21 +82,37 @@ namespace LabRobOPAME3
                 if (choice == "1")
                 {
                     Console.WriteLine("Меню товарів:");
-                    ShowAll(products);
+                    ShowTable();
                 }
                 else if (choice == "2")
                 {
-                    Buy(products);
+                    Buy();
                 }
                 else if (choice == "3")
                 {
-                    Statistics(products);
+                    FullStatistics();
                 }
                 else if (choice == "4")
                 {
-                    Report(products);
+                    AddProduct();
                 }
                 else if (choice == "5")
+                {
+                   SearchProduct();
+                }
+                else if (choice == "6")
+                {
+                    DeleteProduct();
+                }
+                else if (choice == "7")
+                {
+                    SortByName();
+                }
+                else if (choice == "8")
+                {
+                    BubbleSortByPrice();
+                }
+                else if (choice == "0")
                 {
                     Console.WriteLine("Вихід...");
                     return;
@@ -108,23 +124,107 @@ namespace LabRobOPAME3
             }
         }
 
-        private static void ShowAll(Product[] products)
+        public static void AddProduct()
         {
-            for (int i = 0; i < products.Length; i++)
+            Console.WriteLine("Введіть назву товару");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Задайте ціну продукту");
+            string price = Console.ReadLine();
+
+            Console.WriteLine("Введіть кількість");
+            string quantity = Console.ReadLine();
+
+            Console.WriteLine("Введіть категорію товару");
+            string category = Console.ReadLine();
+
+
+            if (string.IsNullOrEmpty(name) || string.IsNullOrEmpty(price) || string.IsNullOrEmpty(quantity) || string.IsNullOrEmpty(category))
             {
-                Console.WriteLine($"{i + 1}. {products[i].Name} - {products[i].Price} грн/шт");
+                Console.WriteLine("Упс! Якесь з полів пусте або всі пусті");
+                Console.WriteLine("Спробуйте знову");
+                return;
             }
+
+            if (double.TryParse(price,out double newPrice) && int.TryParse(quantity, out int newQuantity))
+            {
+                Console.WriteLine("Успішне додавання");
+                products.Add(new Product {Name = name,Price = newPrice,Quantity = newQuantity,Category = category });
+            }
+            else
+            {
+                Console.WriteLine("Упс! Схоже, що ви ввели для ціни або кількості не числові значення");
+                Console.WriteLine("Спробуйте знову");
+                return;
+            }
+
         }
 
-        private static void Buy(Product[] products)
+        private static void SearchProduct()
         {
-            ShowAll(products);
+            Console.Write("\nВведіть назву для пошуку: ");
+            string key = Console.ReadLine().ToLower();
+
+            for (int i = 0; i < products.Count; i++)
+            {
+                if (products[i].Name.ToLower().Contains(key))
+                {
+                    Console.WriteLine($"\nЗнайдено товар #{i + 1}: {products[i].Name}, {products[i].Price} грн, {products[i].Quantity} шт, категорія: {products[i].Category}");
+                    return;
+                }
+            }
+
+            Console.WriteLine("Товар не знайдено.");
+        }
+
+        private static void DeleteProduct()
+        {
+            ShowTable();
+
+            Console.Write("\nВведіть номер товару для видалення: ");
+            if (!int.TryParse(Console.ReadLine(), out int index) || index < 1 || index > products.Count)
+            {
+                Console.WriteLine("Некоректний номер!");
+                return;
+            }
+
+            products.RemoveAt(index - 1);
+            Console.WriteLine("Товар видалено.");
+        }
+
+        private static void SortByName()
+        {
+            products.Sort((a, b) => a.Name.CompareTo(b.Name));
+            Console.WriteLine("Сортування за назвою виконано.");
+        }
+
+        private static void BubbleSortByPrice()
+        {
+            
+            for (int i = 0; i < products.Count - 1; i++)
+            {
+                for (int j = 0; j < products.Count - i - 1; j++)
+                {
+                    if (products[j].Price > products[j + 1].Price)
+                    {
+                        Product temp = products[j];
+                        products[j] = products[j + 1];
+                        products[j + 1] = temp;
+                    }
+                }
+            }
+
+            Console.WriteLine("Бульбашкове сортування за ціною виконано.");
+        }
+        private static void Buy()
+        {
+            ShowTable();
 
             int chooseProduct;
             while (true)
             {
                 Console.Write("\nВведіть номер товару: ");
-                if (int.TryParse(Console.ReadLine(), out chooseProduct) && chooseProduct >= 1 && chooseProduct <= products.Length)
+                if (int.TryParse(Console.ReadLine(), out chooseProduct) && chooseProduct >= 1 && chooseProduct <= products.Count)
                 {
                     chooseProduct--;
                     break;
@@ -162,37 +262,45 @@ namespace LabRobOPAME3
             Console.WriteLine("\nДякуємо за покупку у 'StyleRoom'!");
         }
 
-        private static void Statistics(Product[] arr)
+        private static void FullStatistics()
         {
-            double sum = 0;
-            double min = arr[0].Price;
-            double max = arr[0].Price;
-
-            for (int i = 0; i < arr.Length; i++)
+            if (products.Count == 0)
             {
-                sum += arr[i].Price;
-
-                if (arr[i].Price < min) min = arr[i].Price;
-                if (arr[i].Price > max) max = arr[i].Price;
+                Console.WriteLine("Немає товарів.");
+                return;
             }
 
-            double avg = sum / arr.Length;
+            double sum = 0;
+            double min = products[0].Price;
+            double max = products[0].Price;
+
+            for (int i = 0; i < products.Count; i++)
+            {
+                sum += products[i].Price;
+                if (products[i].Price < min) min = products[i].Price;
+                if (products[i].Price > max) max = products[i].Price;
+            }
+
+            double avg = sum / products.Count;
 
             Console.WriteLine("\nСТАТИСТИКА");
-            Console.WriteLine($"Загальна сума цін: {sum} грн");
-            Console.WriteLine($"Середня ціна: {avg} грн");
-            Console.WriteLine($"Мінімальна цiна: {min} грн");
-            Console.WriteLine($"Максимальна цiна: {max} грн");
+            Console.WriteLine($"Кількість товарів: {products.Count}");
+            Console.WriteLine($"Сума цін: {sum}");
+            Console.WriteLine($"Середня ціна: {avg:F2}");
+            Console.WriteLine($"Мінімальна ціна: {min}");
+            Console.WriteLine($"Максимальна ціна: {max}");
         }
 
-        private static void Report(Product[] products)
+        private static void ShowTable()
         {
-            Console.WriteLine("ЗВІТ");
-            Console.WriteLine("Список всіх товарів");
-            ShowAll(products);
-            Console.WriteLine("Підсумкові дані:");
-            Statistics(products);
-            Console.WriteLine("КІНЕЦЬ ЗВІТУ");
+            Console.WriteLine("\n№ | Назва            | Ціна   | Кількість | Категорія");
+            Console.WriteLine("---------------------------------------------------------");
+
+            for (int i = 0; i < products.Count; i++)
+            {
+                Console.WriteLine($"{i + 1,-2}| {products[i].Name,-15} | {products[i].Price,7} | {products[i].Quantity,9} | {products[i].Category}");
+            }
         }
+
     }
 }
